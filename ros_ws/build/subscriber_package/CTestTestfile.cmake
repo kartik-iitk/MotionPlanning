a40e4118f498e00db10492f8b81d72e435a554ef
debrajk22 <debraj2003jsr@gmail.com>
2024-11-30 17:35:52 +0530

    removed log files

--- ros_ws/build/subscriber_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/debraj/Documents/MotionPlanning/ros_ws/src/subscriber_package
-# Build directory: /home/debraj/Documents/MotionPlanning/ros_ws/build/subscriber_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
